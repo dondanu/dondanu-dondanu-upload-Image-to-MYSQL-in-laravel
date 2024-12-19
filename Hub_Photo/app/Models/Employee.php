26b0d0f72696e37b6342613f281d0c29fd6807fe
Thamili | தமிழி <121654749+dondanu@users.noreply.github.com>
2024-12-19 21:02:23 +0530

    assign controler, create model, connect with db

--- Hub_Photo/app/Models/Employee.php
+++ Hub_Photo/app/Models/Employee.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $table = 'employees';
+    protected $primarykey = 'id';
+    protected $fillable = ['name', 'address', 'mobile', 'gender', 'index','photo' ];
+    use HasFactory;
 }
